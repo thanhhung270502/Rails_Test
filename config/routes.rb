6a3c6447376d75d766818c94e51241d59e16a912
thanhhung270502 <thanhhung270502@gmail.com>
2022-07-21 13:28:02 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # GET /about
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "about-us", to: "about#index", as: :about
+
+  root to: "main#index"
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
